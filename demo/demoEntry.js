a767287e5c915b5f8723858830ffedf8564fb4a6
Phil Plencner <philip_plencner@harvard.edu>
2025-11-06 12:15:57 -0500

    LTSVIEWER-377 Adding Citation Plugin and example manifests.

--- demo/demoEntry.js
+++ demo/demoEntry.js
@@ -1,5 +1,6 @@
 import Mirador from "mirador/dist/es/src/index"
 import Plugin from "../src/index"
+import miradorCitationPlugin from "@harvard-lts/mirador-citation-plugin";
 
 document.addEventListener("DOMContentLoaded", () => {
   const config = {
@@ -8,13 +9,34 @@ document.addEventListener("DOMContentLoaded", () => {
       {
         // manifestId: "data/manifest-3037.json" // EDA manifest with two transcriptions
         // manifestId: "https://iiif.lib.harvard.edu/manifests/ids:488815" // non-EDA manifest
+        // Static local manifest files for testing
         manifestId: "data/manifest-609.json" // EDA manifest with one transcription
         // manifestId: "data/manifest-5f3b.json" // EDA manifest with two transcriptions on one edition
-      }
-    ]
+      },
+      /*{
+        // Manifest on Dev with EDA transcriptions
+        manifestId: "https://mps-dev.lib.harvard.edu/iiif/3/URN-3:HUL.EDA:C4837"
+      },*/
+      /*{
+        // Manifest on Dev with non-EDA transcriptions
+        manifestId: "https://nrs-dev.lib.harvard.edu/URN-3:RAD.SCHL:101118966:MANIFEST:3"
+      }*/
+    ],
+    translations: { // These can be removed when removing the citation plugin
+      "en": {
+        "openCompanionWindow_CitationKey": "Cite",
+        "openCompanionWindow_RelatedLinksKey": "Related Links",
+    },
+    miradorCitationPlugin: {
+      citationAPI: 'https://mps.lib.harvard.edu/citation/'
+    },
+  },
   }
 
-  const plugins = [...Plugin]
+  const plugins = [
+    ...Plugin,
+    miradorCitationPlugin,
+  ]
 
   // Mirador dispatches an action to load the manifest into the global state
   Mirador.viewer(config, plugins)
